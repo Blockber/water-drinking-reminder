0099d993bcca94b9b5da11e0c191cabfc660a092
Blockber <82634585+Blockber@users.noreply.github.com>
2021-09-01 16:56:03 +0800

    Delete install.py

--- install.py
+++
@@ -1,6 +0,0 @@
-from shutil import copy
-from pathlib import Path
-from os.path import dirname
-
-startup = str(Path.home()) + "\\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup"
-copy(dirname(__file__) + "\\drink.exe", startup)
